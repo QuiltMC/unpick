1ce62c00ba4c16fb86cab04d996b2c623c44ade6
Daomephsta <dtnilfisk@gmail.com>
2021-11-22 17:22:03 +1300

    Add missing test constants

--- src/test/java/daomephsta/unpick/tests/ConstantSource.java
+++ src/test/java/daomephsta/unpick/tests/ConstantSource.java
@@ -10,7 +10,11 @@ class ConstantSource
 							 BYTE_CONST_3 = 3,
 							 BYTE_CONST_4 = 4,
 							 BYTE_CONST_5 = 5,
-							 BYTE_CONST = 117;
+							 BYTE_CONST = 117,
+							 BYTE_FLAG_BIT_0 = 1 << 0,
+							 BYTE_FLAG_BIT_1 = 1 << 1,
+							 BYTE_FLAG_BIT_2 = 1 << 2,
+							 BYTE_FLAG_BIT_3 = 1 << 3;
 
 	public static final short SHORT_CONST_M1 = -1,
 							  SHORT_CONST_0 = 0,
@@ -19,7 +23,11 @@ class ConstantSource
 							  SHORT_CONST_3 = 3,
 							  SHORT_CONST_4 = 4,
 							  SHORT_CONST_5 = 5,
-							  SHORT_CONST = 257;
+							  SHORT_CONST = 257,
+							  SHORT_FLAG_BIT_0 = 1 << 0,
+							  SHORT_FLAG_BIT_1 = 1 << 1,
+							  SHORT_FLAG_BIT_2 = 1 << 2,
+							  SHORT_FLAG_BIT_3 = 1 << 3;
 
 	public static final char CHAR_CONST_0 = '\0',
 							 CHAR_CONST_1 = '\1',
@@ -36,11 +44,19 @@ class ConstantSource
 							INT_CONST_3 = 3,
 							INT_CONST_4 = 4,
 							INT_CONST_5 = 5,
-							INT_CONST = 257;
+							INT_CONST = 257,
+							INT_FLAG_BIT_0 = 1 << 0,
+							INT_FLAG_BIT_1 = 1 << 1,
+							INT_FLAG_BIT_2 = 1 << 2,
+							INT_FLAG_BIT_3 = 1 << 3;
 
 	public static final long LONG_CONST_0 = 0,
 							 LONG_CONST_1 = 1,
-							 LONG_CONST = 1234567890;
+							 LONG_CONST = 1234567890,
+							 LONG_FLAG_BIT_0 = 1 << 0,
+							 LONG_FLAG_BIT_1 = 1 << 1,
+							 LONG_FLAG_BIT_2 = 1 << 2,
+							 LONG_FLAG_BIT_3 = 1 << 3;
 
 	public static final float FLOAT_CONST_0 = 0F,
 							  FLOAT_CONST_1 = 1F,
